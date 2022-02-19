b3bc511e599549e0f5c8427b13166d583871fa3a
Kevin <kevin94singh@gmail.com>
2022-02-19 18:36:06 +0100

    Showing error updated

--- TestingRepository/Model/Error/CustomError.swift
+++ TestingRepository/Model/Error/CustomError.swift
@@ -1,23 +1,65 @@
 import Foundation
 
 enum CustomError: Error {
-    case noNetwork
+    case badRequest
     case dataNotFound
     case unknown
+    case sessionNotExtists
+    case noNetwork
     case error(String)
+    case unauthorized
+    case methodNotAllowed
 }
 
 extension CustomError: LocalizedError {
-    public var errorDescription: String? {
+    public var customDescription: (String, String) {
         switch self {
-        case .noNetwork:
-            return "No network"
+        
+        case .badRequest:
+            return ("ERROR", "BAD REQUEST")
         case .dataNotFound:
-            return "Data not found"
-        case .error(let error):
-            return error
+            return ("ERROR", "DATA NOT FOUND")
         case .unknown:
-            return "Unknown error"
+            return ("ERROR", "UNKNOWN ERROR")
+        case .sessionNotExtists:
+            return ("ERROR", "SESSION NOT EXSISTS")
+        case .noNetwork:
+            return ("ERROR", "NO NETWORK")
+        case .error(let error):
+            return ("ERROR", error)
+        case .unauthorized:
+            return ("ERROR", "UNAUTHORIZED")
+        case .methodNotAllowed:
+            return ("ERROR", "METHOD NOT ALLOWED")
         }
+    }
+}
+
+extension CustomError {
+    var rawValue: Int {
+        switch self {
+        case .badRequest:
+            return 1
+        case .dataNotFound:
+            return 2
+        case .unknown:
+            return 3
+        case .sessionNotExtists:
+            return 4
+        case .noNetwork:
+            return 5
+        case .error(_):
+            return 6
+        case .unauthorized:
+            return 7
+        case .methodNotAllowed:
+            return 8
+        }
+    }
+}
+
+extension CustomError: Equatable {
+    static func == (lhs: CustomError, rhs: CustomError) -> Bool {
+        return lhs.rawValue == rhs.rawValue
     }
 }
